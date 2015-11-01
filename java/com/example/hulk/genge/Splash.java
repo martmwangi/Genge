ffe7359b69a951b2797dc216cc88efb8ac2ef4a7
martmwangi <luthermwangi2@gmail.com>
2015-11-01 22:42:00 +0300

    Splash screen works

--- java/com/example/hulk/genge/Splash.java
+++ java/com/example/hulk/genge/Splash.java
@@ -1,28 +1,31 @@
 package com.example.hulk.genge;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Splash extends AppCompatActivity {
+    public static int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this,LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        },SPLASH_TIME_OUT);
+
+
     }
 
 }
