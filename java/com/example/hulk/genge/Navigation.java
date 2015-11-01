adc87f5bdd535f92b905fbe1072d1c96b7198698
martmwangi <luthermwangi2@gmail.com>
2015-11-01 22:59:08 +0300

    Completed part of the layering

--- java/com/example/hulk/genge/Navigation.java
+++ java/com/example/hulk/genge/Navigation.java
@@ -1,11 +1,13 @@
 package com.example.hulk.genge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Navigation extends AppCompatActivity {
 
@@ -15,6 +17,17 @@ public class Navigation extends AppCompatActivity {
         setContentView(R.layout.activity_navigation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button btn1 = (Button) findViewById(R.id.imageButton);
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Navigation.this, Music.class);
+
+            }
+        });
+
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
